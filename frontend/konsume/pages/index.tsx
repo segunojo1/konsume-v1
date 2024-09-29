a3675e65f4e10cb5fe9b241bb9b49d8f6b9bc2dd
segunojo1 <olusegunkd2017@gmail.com>
2024-09-30 00:31:42 +0100

    feat: enable analytics

--- frontend/konsume/pages/index.tsx
+++ frontend/konsume/pages/index.tsx
@@ -3,6 +3,7 @@ import HomeBody from '@/modules/home/HomeBody';
 import HomeNav from '@/modules/home/HomeNav';
 import { Steps } from 'intro.js-react';
 import Link from 'next/link';
+import { Analytics } from "@vercel/analytics/react"
 
 function Home() {
   
@@ -11,6 +12,7 @@ function Home() {
       <div className="font-jakarta md:bg-white bg-[#D6FBC4]  flex flex-col gap-10">
         <HomeNav />
         <HomeBody />
+        <Analytics />
       </div>
     </>
   );
